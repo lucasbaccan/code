9ff84da381188583ebf8fb4b2b92229d8270e39a
lucasbaccan <naccabbaccan@hotmail.com>
2020-10-16 00:14:51 +0000

    Deploying to gh-pages from  @ d270e6dd56328b9ddd3de49d053362e51b62c4c8 🚀

---
+++ 35.195fe7bf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{149:function(n,w){}}]);
---
+++ c47f7606.134b9ea2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{82:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"10-2020","name":"10/2020","count":1,"permalink":"/blog/tags/10-2020"}')}}]);
