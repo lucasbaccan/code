b5b79bcbd6b7f1f6ea60833fd6f32398be7aa45e
Lucas Elias Baccan <naccabbaccan@hotmail.com>
2023-05-01 18:12:12 -0300

    Ajustes de permissão

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,3 +11,5 @@ echo "BASE_URL=${BASE_URL}"
 # # Iniciar o servidor e esperar ele subir e depois gerar o pdf
 npm run serve &
 wait-on ${BASE_URL} && ./docker/gerar_pdf.sh ${BASE_URL}
+
+chmod -R 777 /app/build
