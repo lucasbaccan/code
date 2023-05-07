640ab9582382b11f0b79aece33eed94b2fed3a63
Lucas Elias Baccan <naccabbaccan@hotmail.com>
2023-05-07 19:47:50 -0300

    Ajustes para corrigir fontes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,11 @@
 
 chmod +x /app/docker/gerar_pdf.sh
 
+dos2unix /app/docker/gerar_pdf.sh
+
 git config --global --add safe.directory /app
+
+npm install mr-pdf-args
 
 npm install --no-progress -g wait-on
 
@@ -10,6 +14,6 @@ echo "BASE_URL=${BASE_URL}"
 
 # # Iniciar o servidor e esperar ele subir e depois gerar o pdf
 npm run serve &
-wait-on ${BASE_URL} && ./docker/gerar_pdf.sh ${BASE_URL}
+wait-on ${BASE_URL} && /app/docker/gerar_pdf.sh ${BASE_URL}
 
 chmod -R 777 /app/pdf
