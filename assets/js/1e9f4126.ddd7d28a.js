d26f5e7529f9d9372995b1aeea59ef1e0926ce85
lucasbaccan <naccabbaccan@hotmail.com>
2024-03-23 23:33:00 +0000

    Deploying to gh-pages from  @ 2063a63c0cd3132365b1402a50b6a93a72bdde02 🚀

---
+++ assets/js/1e9f4126.ddd7d28a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcode=self.webpackChunkcode||[]).push([[9971],{2494:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/baccan","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/35d82d60.5f6cbeee.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcode=self.webpackChunkcode||[]).push([[1850],{1907:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/12-2023","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
